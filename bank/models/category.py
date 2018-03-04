915cd537635d1f4b576e56fb887fd3f3b29a4f1a
manu <mechin.emmanuel@gmail.com>
2018-03-04 20:10:48 +0100

    total category

--- bank/models/category.py
+++ bank/models/category.py
@@ -1,8 +1,16 @@
-from django.db.models import Model, fields
+from django.db.models import Model, fields, aggregates
 
 
 class Category(Model):
     name = fields.CharField(max_length=1000)
+
+    @property
+    def total(self):
+        from .transaction import Transaction
+        total = Transaction.objects.filter(category=self).aggregate(total_value=aggregates.Sum('value'))['total_value']
+        if total is None:
+            return 0
+        return round(total, 2)
 
     def __str__(self):
         return 'Category: {name}'.format(name=self.name)
