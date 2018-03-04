efc7985f113bf284a16cc76c2a6fbbebbb466ca2
manu <mechin.emmanuel@gmail.com>
2018-03-04 20:11:15 +0100

    category matching fct

--- bank/models/transaction.py
+++ bank/models/transaction.py
@@ -2,6 +2,7 @@ from django.db.models import Model, fields
 from django.db.models.fields.related import ForeignKey, CASCADE
 
 from .account import Account
+from .category import Category
 
 
 class Transaction(Model):
@@ -9,6 +10,14 @@ class Transaction(Model):
     date = fields.DateTimeField('date')
     label = fields.CharField(max_length=1000)
     value = fields.FloatField()
+    category = ForeignKey(Category, null=True)
+
+    def categorize(self, category_matching):
+        for m, c in category_matching:
+            if m in self.label:
+                self.category = c
+                self.save()
+                break
 
     class Meta:
         unique_together = (('account', 'date', 'label', 'value'), )
